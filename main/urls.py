0c4ead139d0c1263408b2339c13d4e64b3185bac
Andy Hua <user.andy.hua@gmail.com>
2023-02-21 14:08:35 -0800

    static files fix

--- main/urls.py
+++ main/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('account/', include("accounts_app.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += \
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
